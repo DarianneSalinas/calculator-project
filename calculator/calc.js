373835e7cfbd4d27b216f10845875a741b7c345e
DarianneSalinas <darianne.salinas@gmail.com>
2022-03-22 16:05:00 -0600

    Finished calculator

--- calculator/calc.js
+++ calculator/calc.js
@@ -29,6 +29,10 @@ reader.question("What would you like to calculate?", function(input){
 	  
 				 } else if (mathSymbol === "/"){
 					console.log(num1 / num2);
+
+				} else if (mathSymbol === "sqrt"){
+					console.log(Math.sqrt(num1));
+					console.log(Math.sqrt(num2));
 				}
 	
 
